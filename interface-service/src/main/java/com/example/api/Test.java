97999ad09d1ba477cac3293e924ab5a2c47c6c12
吴佳杰 <827121759@qq.com>
2023-01-09 09:57:34 +0800

    打包到仓库

---
+++ interface-service/src/main/java/com/example/api/Test.java
@@ -0,0 +1,17 @@
+package com.example.api;
+
+/**
+ * @author 吴佳杰
+ * @date 2023/1/9 09:51
+ */
+public class Test {
+    /**
+     * test
+     * @param name
+     * @return
+     */
+    public static String test(String name) {
+        // 返回
+        return name + "123";
+    }
+}
